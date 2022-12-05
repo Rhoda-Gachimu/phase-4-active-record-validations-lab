826ee7467b78a40c917540b0f083f8e0a2ed53b0
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-12-06 02:32:45 +0300

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  # validate :title_is_clickbait
+
+  def title_is_clickbait
+    # words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+    # unless words.any? { |word| title.include? word }
+    #   errors.add(:title, "Title not valid") 
+    # end
+
+    unless title.include?("Won't Believe") or title.include?("Secret") or title.include?("Top [number]") or title.include?("Guess")
+      errors.add(:title, "Title not valid") 
+    end
+  end
 end
